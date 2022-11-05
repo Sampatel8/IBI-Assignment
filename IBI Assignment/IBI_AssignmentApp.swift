1e1767fa7f25da88a79a76026e40c3b101d6cee4
Smit Patel <34744262+Sampatel8@users.noreply.github.com>
2022-11-05 16:44:53 -0400

    Initial Commit.

---
+++ IBI Assignment/IBI_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  IBI_AssignmentApp.swift
+//  IBI Assignment
+//
+//  Created by Smit Patel on 2022-11-05.
+//
+
+import SwiftUI
+
+@main
+struct IBI_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
